e60635146222b36b3ea65c872add3bc93c21918e
scrolls5 <hh.konuksever@live.nl>
2022-11-01 10:21:59 +0100

    Update firebase.py

---
+++ Database/firebase.py
@@ -0,0 +1,42 @@
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("Project_week/mtisrb-firebase-adminsdk-u1zpn-c585cb5c8c.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mtisrb-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference('py/')
+users_ref = ref.child('users')
+users_ref.set({
+    'gebruiker_1': {
+        'user_id': '0',
+        'voornaam': 'John',
+        'achternaam': 'Doe'
+    }
+})
+
+vragen_ref = ref.child('vragen')
+vragen_ref.set({
+    'vraag_1': {
+        'vraag_id': '1',
+        'vraag': 'In welke richting heb je al interesse?'
+    }
+})
+
+antwoorden_ref = ref.child('antwoorden')
+antwoorden_ref.set({
+    'antwoord_1': {
+        'antwoorden_id': '1',
+        'antwoorden': 'Forensische ICT, Interactie-Technologie, Software Engineering, Data engineer, geen idee'
+    }
+})
+
+# hopper_ref = users_ref.child('gebruiker')
+
+handle = db.reference('py/users/')
+
+print(ref.get())
