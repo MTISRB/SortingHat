6268a5c08a4ae73610c8f925f14247aa41d6bc68
scrolls5 <hh.konuksever@live.nl>
2022-11-01 11:04:52 +0100

    Merge branch 'main' of https://github.com/MTISRB/SortingHat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Database.firebase as f
 
 
+# PLACE ALL YOUR CODE TO RUN/TEST HERE!
 def main():
     f.parse("Database/key/mtisrb-firebase-adminsdk-u1zpn-c585cb5c8c.json")
     my_tup = f.query()
