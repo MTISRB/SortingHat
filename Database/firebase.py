4c97ba2e442babdd3ab0a7beec5d436f805d2e70
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-01 11:39:41 +0100

    upgrade

--- Database/firebase.py
+++ Database/firebase.py
@@ -17,8 +17,23 @@ def parse(key):
 
 
 def load_data(xlsx_file, sheet_name) -> dict:
+    def get_nth_key(dictionary, n=0):
+        if n < 0:
+            n += len(dictionary)
+
+        for i, key in enumerate(dictionary.keys()):
+            if i == n:
+                return key
+
     # Dictionary in list with lists as pairs and excel columns as keys
-    data = {}
+    data = {
+        "ID": [],
+        "Vragen": [],
+        "Antwoorden": [],
+        "Studierichting": [],
+        "Punten": []
+    }
+    
     excel = pd.read_excel(xlsx_file, sheet_name=sheet_name)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
