c47834bd9a945fb4ff7597e2501bd1a13612a0fe
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-01 16:20:18 +0100

    Update screens.py

--- Screens/screens.py
+++ Screens/screens.py
@@ -3,16 +3,60 @@ import tkinter.constants as tk_cons
 
 
 class Window:
-    pass
+    master: tk.Tk
+    width: int = 0
+    height: int = 0
+
+    def __init__(self, master, title):
+        self.master = master
+        # add title, width, height etc.
+
+    def init_components(self):
+        # override this function in child classes
+        pass
+
+    def _new(self, _class):
+        self.new = tk.Toplevel(self.master)
+        _class(self.new)
 
 
-class BeginWindow:
-    pass
+class BeginWindow(Window):
+    root: tk.Tk
+
+    def __init__(self, root):
+        super().__init__(root, "[Title here!]")
+        self.root = root
+        # add title, width, height etc.
+
+    def init_components(self):
+        super().init_components()
+        # override this function in child classes
 
 
-class EndWindow:
-    pass
+class EndWindow(Window):
+    root: tk.Tk
+
+    def __init__(self, root):
+        super().__init__(root, "[Title here!]")
+        self.root = root
+        # add title, width, height etc.
+
+    def init_components(self):
+        super().init_components()
+        # override this function in child classes
 
 
-class QuestionWindow:
-    pass
+class QuestionWindow(Window):
+    root: tk.Tk
+
+    def __init__(self, root):
+        super().__init__(root, "[Title here!]")
+        self.root = root
+        # add title, width, height etc.
+
+    def init_components(self):
+        super().init_components()
+        # override this function in child classes
+
+    def correct(self) -> bool:
+        pass
