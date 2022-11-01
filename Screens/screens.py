29469c106784063248838a9679dbc726fea48249
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-01 16:08:37 +0100

    Create screens.py

---
+++ Screens/screens.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import tkinter.constants as tk_cons
+
+
+class Window:
+    pass
+
+
+class BeginWindow:
+    pass
+
+
+class EndWindow:
+    pass
+
+
+class QuestionWindow:
+    pass
