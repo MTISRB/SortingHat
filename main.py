ea5a63f1a1a29cf2499cb85f2be8143d98035f0b
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-02 15:49:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import Database as f
+import algorithm as a
 
 
 # PLACE ALL YOUR CODE TO RUN/TEST HERE!
 def main():
-    f.parse('../Firebase key/mtisrb-firebase-adminsdk-u1zpn-13e20fa0ad.json')
-    f.fill_fb(f.load_data("resources/Database.xlsx", "database"))
+    f.init('../Firebase key/mtisrb-firebase-adminsdk-u1zpn-13e20fa0ad.json', fill=True)
+    # for i in range(15):
+    #     f.upload("user_answers", i, f"Hello world {i}")
+    a.Algorithm.init(data=[
+        f.get_data("answers"),
+        f.get_data("field_of_study"),
+        f.get_data("question"),
+        f.get_data("points"),
+        f.get_data("user_answers"),
+    ])
+
+    a.Algorithm.cs()
 
 
 if __name__ == '__main__':
