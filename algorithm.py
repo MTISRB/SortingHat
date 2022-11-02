65e297b7ad955bab09e936d8250dd97fef0d1158
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-02 13:16:27 +0100

    update

--- algorithm.py
+++ algorithm.py
@@ -1,42 +1,25 @@
-# __list__ = []
-# composed = []
-# composed_2 = []
-# for each in data.values():
-#     __list__.append(each)
-# data_id = __list__[0]
-# vraag = __list__[1]
-# antwoord = __list__[2]
-# richting = __list__[3]
-# punten = __list__[4]
-# # print(data_id)
-# # print(vraag)
-# # print(antwoord)
-# # print(punten)
-# composed.append(richting)
-# composed.append(punten)
-# composed_2.append(composed)
-# for each2 in antwoord:
-#     list(each2)
-#     composed_dict = dict(zip(each2, composed_2))
-#     print(composed_dict)
-
 class Algorithm:
-    d_id: tuple
-    a: tuple
+    answers: tuple
     fos: tuple
-    p: tuple
-    q: tuple
-    ua: tuple
+    points: tuple
+    questions: tuple
+    user_answers: tuple
 
     @staticmethod
     def init(data: list):
-        Algorithm.d_id = data[0]
-        Algorithm.a = data[1]
-        Algorithm.fos = data[2]
-        Algorithm.p = data[3]
-        Algorithm.q = data[4]
-        Algorithm.ua = data[5]
+        Algorithm.answers = data[0]
+        Algorithm.fos = data[1]
+        Algorithm.points = data[3]
+        Algorithm.questions = data[2]
+        Algorithm.user_answers = data[4]
 
     @staticmethod
     def cs():
-        pass
+        scores = []
+
+        sorted_answers = [[] for _ in range(15)]
+        sorted_fos = [[] for _ in range(15)]
+        sorted_points = [[] for _ in range(15)]
+
+        print(sorted_answers)
+
