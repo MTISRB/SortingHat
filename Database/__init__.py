9b6d639749ff437133c8a1eb7d1b24d18737e73e
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-02 11:06:52 +0100

    updated

--- Database/__init__.py
+++ Database/__init__.py
@@ -1,1 +1,16 @@
 from Database.fb_db import fill_fb, load_data, parse, query, reference, upload, print_db, retrieve_data, MAIN_ROOT
+
+
+def init(fb_key: str):
+    parse(fb_key)
+    fill_fb(load_data("resources/Database.xlsx", "database"))
+
+
+def get_data(key) -> tuple:
+    data = retrieve_data()
+    for x in data:
+        if key in x:
+            return x
+    else:
+        print("The key doesn't exist in the database!")
+        return ()
