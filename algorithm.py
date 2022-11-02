4e8a2395af8372c46b8af063b8b21efff202bb46
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-02 12:17:06 +0100

    pushed

--- algorithm.py
+++ algorithm.py
@@ -1,37 +1,42 @@
-dataset = {
-    "ID" : ['0'],
-    "Vraag" : ["In welke richting heb je al interesse?"],
-    "Antwoord" : ["forenische ict", "interachtie-technologie", "software engineer", "data engineer", "geen idee"],
-    "Richting" : ["FICT", "IICT", "SE", "DB"],
-    "Punten" : ["4","4","4","0"]
-}
+# __list__ = []
+# composed = []
+# composed_2 = []
+# for each in data.values():
+#     __list__.append(each)
+# data_id = __list__[0]
+# vraag = __list__[1]
+# antwoord = __list__[2]
+# richting = __list__[3]
+# punten = __list__[4]
+# # print(data_id)
+# # print(vraag)
+# # print(antwoord)
+# # print(punten)
+# composed.append(richting)
+# composed.append(punten)
+# composed_2.append(composed)
+# for each2 in antwoord:
+#     list(each2)
+#     composed_dict = dict(zip(each2, composed_2))
+#     print(composed_dict)
 
-user_input = "data engineer"
+class Algorithm:
+    d_id: tuple
+    a: tuple
+    fos: tuple
+    p: tuple
+    q: tuple
+    ua: tuple
 
-def algorithm(data: dict,user_input):
-    __list__ = []
-    composed = []
-    composed_2 = []
-    for each in data.values():
-        __list__.append(each)
-    data_id = __list__[0]
-    vraag = __list__[1]
-    antwoord = __list__[2]
-    richting = __list__[3]
-    punten = __list__[4]
-    # print(data_id)
-    # print(vraag)
-    # print(antwoord)
-    # print(richting)
-    # print(punten)
-    composed.append(richting)
-    composed.append(punten)
-    composed_2.append(composed)
-    for each2 in antwoord:
-        list(each2)
-        composed_dict = dict(zip(each2, composed_2))
-        print(composed_dict)
+    @staticmethod
+    def init(data: list):
+        Algorithm.d_id = data[0]
+        Algorithm.a = data[1]
+        Algorithm.fos = data[2]
+        Algorithm.p = data[3]
+        Algorithm.q = data[4]
+        Algorithm.ua = data[5]
 
-
-
-algorithm(dataset,user_input)
+    @staticmethod
+    def cs():
+        pass
