3485898702bec2ffb2ab272ee1626d5ca7dd60ea
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-03 09:04:15 +0100

    updated

--- Screens/screens.py
+++ Screens/screens.py
@@ -4,32 +4,38 @@ import PySimpleGUI as sg
 
 
 class LoadQuestion:
+    def __init__(self):
+        pass
 
-    def questionlayout(self, vraag, ant1, ant2, ant3, ant4):
-        kop = ("Helvetica", 25)
-        platte_tekst = ("Helvetica", 15)
+    @staticmethod
+    def question_layout(question, ant1, ant2, ant3, ant4):
+        font = ("Helvetica", 25)
+        font_s = ("Helvetica", 15)
 
         col1 = [[sg.Image('resources/img/sorting_hat-3.png')]]
-        col2 = [[sg.Text(vraag, key='-text-', font=kop)],
-                [sg.Radio(ant1, "RADIO1", key="-Antwoord1-", font=platte_tekst)],
-                [sg.Radio(ant2, "RADIO1", key="-Antwoord2-", font=platte_tekst)],
-                [sg.Radio(ant3, "RADIO1", key="-Antwoord3-", font=platte_tekst)],
-                [sg.Radio(ant4, "RADIO1", key="-Antwoord4-", font=platte_tekst)],
-                [sg.Button('< Terug', font=platte_tekst, size=7),
-                 sg.Button('Verder >', font=platte_tekst, size=7, key="-EnterQuestion-")]]
+        col2 = [[sg.Text(question, key='-text-', font=font)],
+                [sg.Radio(ant1, "RADIO1", key="-Antwoord1-", font=font_s)],
+                [sg.Radio(ant2, "RADIO1", key="-Antwoord2-", font=font_s)],
+                [sg.Radio(ant3, "RADIO1", key="-Antwoord3-", font=font_s)],
+                [sg.Radio(ant4, "RADIO1", key="-Antwoord4-", font=font_s)],
+                [sg.Button('< Terug', font=font_s, size=7),
+                 sg.Button('Verder >', font=font_s, size=7, key="-EnterQuestion-")]]
 
         return col1, col2
 
 
 class LoadResults:
+    def __init__(self):
+        pass
 
-    def resultlayout(self, result):
-        kop_text = ("Helvetica", 25)
-        platte_text = ("Helvetica", 15)
+    @staticmethod
+    def result_layout(result):
+        font = ("Helvetica", 25)
+        font_s = ("Helvetica", 15)
 
         col1 = [[sg.Image("resources/img/sorting_hat-2.png")]]
-        col2 = [[sg.Text('Je resultaat is binnen', font=kop_text)],
-                [sg.Text('Jij past bij:', font=platte_text), sg.Text(result, font=platte_text)],
+        col2 = [[sg.Text('Je resultaat is binnen', font=font)],
+                [sg.Text('Jij past bij:', font=font_s), sg.Text(result, font=font_s)],
                 [sg.Button('Sluiten', key="-close-")]]
 
         return col1, col2
