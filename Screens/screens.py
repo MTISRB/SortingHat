7281c1c7b45256c6e2638fd8532addf66040bb2c
MTISRB <117155537+MTISRB@users.noreply.github.com>
2022-11-03 18:39:21 +0100

    update

--- Screens/screens.py
+++ Screens/screens.py
@@ -2,22 +2,25 @@ import PySimpleGUI as sg
 
 
 class LoadQuestion:
+    counter: int = 0
+
     def __init__(self):
         pass
 
     @staticmethod
-    def question_layout(question, ant1, ant2, ant3, ant4):
+    def question_layout(id, question, ant1, ant2, ant3, ant4):
+        LoadQuestion.counter += 1
         font = ("Helvetica", 25)
         font_s = ("Helvetica", 15)
 
         col1 = [[sg.Image('resources/img/sorting_hat-3.png')]]
         col2 = [[sg.Text(question, key='-text-', font=font)],
-                [sg.Radio(ant1, "RADIO1", key="-Antwoord1-", font=font_s)],
-                [sg.Radio(ant2, "RADIO1", key="-Antwoord2-", font=font_s)],
-                [sg.Radio(ant3, "RADIO1", key="-Antwoord3-", font=font_s)],
-                [sg.Radio(ant4, "RADIO1", key="-Antwoord4-", font=font_s)],
-                [sg.Button('< Terug', font=font_s, size=7),
-                 sg.Button('Verder >', font=font_s, size=7, key="-EnterQuestion-")]]
+                [sg.Radio(ant1, "RADIO1", key=f"-Antwoord{id}-", font=font_s)],
+                [sg.Radio(ant2, "RADIO1", key=f"-Antwoord{id+1}-", font=font_s)],
+                [sg.Radio(ant3, "RADIO1", key=f"-Antwoord{id+2}-", font=font_s)],
+                [sg.Radio(ant4, "RADIO1", key=f"-Antwoord{id+3}-", font=font_s)],
+                [sg.Button('< Terug', font=font_s, size=7, key="-Back-"),
+                 sg.Button('Verder >', font=font_s, size=7, key="-Next-")]]
 
         return col1, col2
 
@@ -40,6 +43,8 @@ class LoadResults:
 
 
 class Content:
+    index: int = 0
+
     def __init__(self):
         pass
 
@@ -47,7 +52,7 @@ class Content:
     def draw_screens(q_col1, q_col2, r_col1, r_col2):
         screens = [[
             sg.Frame('', [
-                [sg.Image('resources/img/sorting_hat.png')],
+                [sg.Image('resources/img/sorting_hat-2.png')],
                 [sg.Text('Welkom bij de Sorting Experience! Voer hieronder je naam in!')],
                 [sg.Input(key="-name-")],
                 [sg.Button("Begin de Sorting Experience!", key="-beginExperience-")]],
@@ -55,21 +60,105 @@ class Content:
 
             sg.Frame('', [
                 [sg.VPush()],
-                [sg.Push(), sg.Column(q_col1), sg.Column(q_col2), sg.Push()],
+                [sg.Push(), sg.Column(q_col1[0]), sg.Column(q_col2[0]), sg.Push()],
                 [sg.VPush()]],
-                     visible=False, border_width=0, element_justification='c', key="-questions-"),
+                     visible=False, border_width=0, element_justification='c', key="-questions_1-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[1]), sg.Column(q_col2[1]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_2-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[2]), sg.Column(q_col2[2]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_3-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[3]), sg.Column(q_col2[3]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_4-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[4]), sg.Column(q_col2[4]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_5-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[5]), sg.Column(q_col2[5]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_6-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[6]), sg.Column(q_col2[6]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_7-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[7]), sg.Column(q_col2[7]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_8-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[8]), sg.Column(q_col2[8]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_9-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[9]), sg.Column(q_col2[9]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_10-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[10]), sg.Column(q_col2[10]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_11-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[11]), sg.Column(q_col2[11]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_12-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[12]), sg.Column(q_col2[12]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_13-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[13]), sg.Column(q_col2[13]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_14-"),
+
+            sg.Frame('', [
+                [sg.VPush()],
+                [sg.Push(), sg.Column(q_col1[14]), sg.Column(q_col2[14]), sg.Push()],
+                [sg.VPush()]],
+                     visible=False, border_width=0, element_justification='c', key="-questions_15-"),
 
             sg.Frame('', [
                 [sg.VPush()],
                 [sg.Push(), sg.Column(r_col1), sg.Column(r_col2), sg.Push()],
                 [sg.VPush()]],
                      visible=False, border_width=0, element_justification='c', key="-end-")
-        ]
-        ]
+        ]]
+
         return screens
 
     @staticmethod
-    def switchcontent(activewindow, currentc, nextc):
+    def switch_content(activewindow, currentc, nextc):
         """
         # Changes the showing content
         :param activewindow: The window in which the content is placed
@@ -77,6 +166,7 @@ class Content:
         :param nextc: The key of the content-frame you want to display next
         :return:
         """
+
         activewindow[currentc].update(visible=False)
         activewindow[nextc].update(visible=True)
         activewindow.refresh()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import Database as f
 import algorithm as a
 import PySimpleGUI as sg
+import utils
+
 from Screens.screens import LoadQuestion
 from Screens.screens import LoadResults
 from Screens.screens import Content
-
 
 # PLACE ALL YOUR CODE TO RUN/TEST HERE!
 def main():
@@ -26,26 +27,64 @@ def main():
     # f.upload("user_answers", 13, 2)
     # f.upload("user_answers", 14, 3)
 
-    a.Algorithm.init(data=[
-       f.get_data("answers"),
-       f.get_data("field_of_study"),
-       f.get_data("question"),
-       f.get_data("points"),
-       f.get_data("user_answers"),
-    ])
-
-    print(a.Algorithm.cs())
+    # a.Algorithm.init(data=[
+    #    f.get_data("answers"),
+    #    f.get_data("field_of_study"),
+    #    f.get_data("question"),
+    #    f.get_data("points"),
+    #    f.get_data("user_answers"),
+    # ])
+    #
+    # print(a.Algorithm.cs())
 
     # choose theme
+
+    q_screen = []
+    for _ in range(15):
+        q_screen.append(LoadQuestion)
+
+    sorted_questions = utils.sort(f.get_data("question"), "question", "question_id")
+    sorted_answers = utils.sort(f.get_data("answers"), "answers", "answers_id")
+
     sg.theme('Default1')
 
 #   question screen
-    q_col1, q_col2 = LoadQuestion.question_layout("Hier komt de vraag", "Antwoord 1", "Antwoord 2", "Antwoord 3", "Antwoord 4")
+    q_col1, q_col2 = LoadQuestion.question_layout(1, sorted_questions[0][0], sorted_answers[0][0], sorted_answers[0][1], sorted_answers[0][2], sorted_answers[0][3])
+    q_col3, q_col4 = LoadQuestion.question_layout(5, sorted_questions[1][0], sorted_answers[1][0], sorted_answers[1][1], sorted_answers[1][2], sorted_answers[1][3])
+    q_col5, q_col6 = LoadQuestion.question_layout(9, sorted_questions[2][0], sorted_answers[2][0], sorted_answers[2][1], sorted_answers[2][2], sorted_answers[2][3])
+    q_col7, q_col8 = LoadQuestion.question_layout(13, sorted_questions[3][0], sorted_answers[3][0], sorted_answers[3][1], sorted_answers[3][2], sorted_answers[3][3])
+    q_col9, q_col10 = LoadQuestion.question_layout(17, sorted_questions[4][0], sorted_answers[4][0], sorted_answers[4][1], sorted_answers[4][2], sorted_answers[4][3])
+    q_col11, q_col12 = LoadQuestion.question_layout(21, sorted_questions[5][0], sorted_answers[5][0], sorted_answers[5][1], sorted_answers[5][2], sorted_answers[5][3])
+    q_col13, q_col14 = LoadQuestion.question_layout(25, sorted_questions[6][0], sorted_answers[6][0], sorted_answers[6][1], sorted_answers[6][2], sorted_answers[6][3])
+    q_col15, q_col16 = LoadQuestion.question_layout(29, sorted_questions[7][0], sorted_answers[7][0], sorted_answers[7][1], sorted_answers[7][2], sorted_answers[7][3])
+    q_col17, q_col18 = LoadQuestion.question_layout(33, sorted_questions[8][0], sorted_answers[8][0], sorted_answers[8][1], sorted_answers[8][2], sorted_answers[8][3])
+    q_col19, q_col20 = LoadQuestion.question_layout(37, sorted_questions[9][0], sorted_answers[9][0], sorted_answers[9][1], sorted_answers[9][2], sorted_answers[9][3])
+    q_col21, q_col22 = LoadQuestion.question_layout(41, sorted_questions[10][0], sorted_answers[10][0], sorted_answers[10][1], sorted_answers[10][2], sorted_answers[10][3])
+    q_col23, q_col24 = LoadQuestion.question_layout(45, sorted_questions[11][0], sorted_answers[11][0], sorted_answers[11][1], sorted_answers[11][2], sorted_answers[11][3])
+    q_col25, q_col26 = LoadQuestion.question_layout(49, sorted_questions[12][0], sorted_answers[12][0], sorted_answers[12][1], sorted_answers[12][2], sorted_answers[12][3])
+    q_col27, q_col28 = LoadQuestion.question_layout(53, sorted_questions[13][0], sorted_answers[13][0], sorted_answers[13][1], sorted_answers[13][2], sorted_answers[13][3])
+    q_col29, q_col30 = LoadQuestion.question_layout(57, sorted_questions[14][0], sorted_answers[14][0], sorted_answers[14][1], sorted_answers[14][2], sorted_answers[14][3])
+
     # end sceen
     r_col1, r_col2 = LoadResults.result_layout("Resultaat!")
 
     # content
-    content = Content.draw_screens(q_col1, q_col2, r_col1, r_col2)
+    content = Content.draw_screens(
+        (
+            q_col1, q_col3, q_col5,
+            q_col7, q_col9, q_col11,
+            q_col13, q_col15, q_col17,
+            q_col19, q_col21, q_col23,
+            q_col25, q_col27, q_col29,
+        ),
+        (
+            q_col2, q_col4, q_col6,
+            q_col8, q_col10, q_col12,
+            q_col14, q_col16, q_col18,
+            q_col20, q_col22, q_col24,
+            q_col26, q_col28, q_col30,
+        ),
+        r_col1, r_col2)
 
     # create the window, finalize it and start it at full screen
     window = sg.Window('Sorting Experience', content, element_justification='c',
@@ -53,9 +92,11 @@ def main():
     window.Maximize()
 
     # creates the main window, finalizes it and makes it start in full screen mode
-
     while True:
         event, values = window.read()
+        index = 1
+
+        print(values, event)
 
         if event == sg.WIN_CLOSED:
             break
@@ -79,28 +120,72 @@ def main():
                 entered_name = values["-name-"]
 
             if entered_name != "":
-                Content.switchcontent(window, "-welcome-", "-questions-")
-
-        elif event == "-EnterQuestion-":
+                Content.switch_content(window, "-welcome-", "-questions_1-")
+        elif "Next" in event:
             # dit gedeelte wordt geactiveerd wanneer er op de verder knop gedrukt wordt bij het vragen scherm,
             # en je kan hier zien welk van de vier keuzes gekozen is
-            if values["-Antwoord1-"]:
-                # Geeft aan in de terminal dat het eerste antwoord gekozen is
-                print("Gekozen antwoord is 1")
-            elif values["-Antwoord2-"]:
-                # Geeft aan in de terminal dat het tweede antwoord gekozen is
-                print("Gekozen antwoord is 2")
-            elif values["-Antwoord3-"]:
-                # Geeft aan in de terminal dat het derde antwoord gekozen is
-                print("Gekozen antwoord is 3")
-            elif values["-Antwoord4-"]:
-                # Geeft aan in de terminal dat het vierde antwoord gekozen is
-                print("Gekozen antwoord is 4")
-            Content.switchcontent(window, "-questions-", '-end-')
+            print(True, Content.index)
+            temp = values.copy()
+            temp.pop("-name-", None)
+            # index = 1
+            for val in temp:
+                if values[f"-Antwoord{index}-"]:
+                    f.upload("user_answers", Content.index, 0)
+                    break
+                else:
+                    index += 1
+                if values[f"-Antwoord{index}-"]:
+                    f.upload("user_answers", Content.index, 1)
+                    break
+                else:
+                    index += 1
+                if values[f"-Antwoord{index}-"]:
+                    f.upload("user_answers", Content.index, 2)
+                    break
+                else:
+                    index += 1
+                if values[f"-Antwoord{index}-"]:
+                    f.upload("user_answers", Content.index, 3)
+                    break
+                else:
+                    index += 1
+
+            Content.index += 1
+            if Content.index == 1:
+                Content.switch_content(window, "-questions_1-", '-questions_2-')
+            elif Content.index == 2:
+                Content.switch_content(window, "-questions_2-", '-questions_3-')
+            elif Content.index == 3:
+                Content.switch_content(window, "-questions_3-", '-questions_4-')
+            elif Content.index == 4:
+                Content.switch_content(window, "-questions_4-", '-questions_5-')
+            elif Content.index == 5:
+                Content.switch_content(window, "-questions_5-", '-questions_6-')
+            elif Content.index == 6:
+                Content.switch_content(window, "-questions_6-", '-questions_7-')
+            elif Content.index == 7:
+                Content.switch_content(window, "-questions_7-", '-questions_8-')
+            elif Content.index == 8:
+                Content.switch_content(window, "-questions_8-", '-questions_9-')
+            elif Content.index == 9:
+                Content.switch_content(window, "-questions_9-", '-questions_10-')
+            elif Content.index == 10:
+                Content.switch_content(window, "-questions_10-", '-questions_11-')
+            elif Content.index == 11:
+                Content.switch_content(window, "-questions_11-", '-questions_12-')
+            elif Content.index == 12:
+                Content.switch_content(window, "-questions_12-", '-questions_13-')
+            elif Content.index == 13:
+                Content.switch_content(window, "-questions_13-", '-questions_14-')
+            elif Content.index == 14:
+                Content.switch_content(window, "-questions_14-", '-questions_15-')
+            elif Content.index == 15:
+                Content.switch_content(window, "-questions_15-", '-end-')
         elif event == "-close-":
             window.close()
         else:
             window.close()
+        values.clear()
 
 
 if __name__ == '__main__':
